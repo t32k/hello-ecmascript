39fc0b6af9f6a0fce98fe7d5cebabeb5ddf8be06
t32k <ijok.ijok@gmail.com>
2015-03-23 23:09:20 -0700

    Parent

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
 $(function() {
-  // $(el).outerWidth(true);
-  // IE9+
+  // $(el).parent();
+  // IE8+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
-
-  function outerWidth(el) {
-    var width = el.offsetWidth;
-    var style = getComputedStyle(el);
-
-    width += parseInt(style.marginLeft) + parseInt(style.marginRight);
-    return width;
-  }
-
-  var text = outerWidth(el);
+  
+  var text = el.parentNode;
   console.log(text);
 });
