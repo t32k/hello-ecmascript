b7df46ef387919a9f26dc073a8894252ecb90338
t32k <ijok.ijok@gmail.com>
2015-03-23 22:54:47 -0700

    Outer Width

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
 $(function() {
-  // $(el).outerHeight(true);
-  // IE9+
+  // $(el).outerWidth();
+  // IE8+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  function outerHeight(el) {
-    var height = el.offsetHeight;
-    var style = getComputedStyle(el);
-
-    height += parseInt(style.marginTop) + parseInt(style.marginBottom);
-    return height;
-  }
-
-  var text = outerHeight(el);
+  var text = el.offsetWidth;
   console.log(text);
 });
