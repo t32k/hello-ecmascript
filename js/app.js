4a4127ad3bb2c4f416d19a01e1565c8bd3596ae6
t32k <ijok.ijok@gmail.com>
2015-03-23 22:50:42 -0700

    Offset Parent

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 $(function() {
-  // $(el).offset();
+  // $(el).offsetParent();
   // IE8+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  var rect = el.getBoundingClientRect();
-  console.log({
-    top: rect.top + document.body.scrollTop,
-    left: rect.left + document.body.scrollLeft
-  });
+  var text = el.offsetParent || el;
+  console.log(text);
 });
