62e4552eaa632a56d8628987965cd939743e5fa4
t32k <ijok.ijok@gmail.com>
2015-03-23 22:29:25 -0700

    Matches Selector

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(function() {
-  // IE8+
-  // $(el).is($(otherEl));
+  // IE9+
+  // $(el).is('.my-class');;
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  el === otherEl;
+  var matches = function(el, selector) {
+    return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
+  };
 
+  matches(el, '.my-class');
 });
