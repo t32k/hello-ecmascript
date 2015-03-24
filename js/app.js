04442f199b25aa99e124c6d2490c90a08ca17382
t32k <ijok.ijok@gmail.com>
2015-03-23 21:30:52 -0700

    Each

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 $(function() {
-  // IE8+
+  // IE9+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
+  var items = document.querySelectorAll('#js-list > li');
 
-  console.log(el.querySelector('#js-child') !== null);
+  Array.prototype.forEach.call(items, function(el, i){
+    console.log(el, i);
+  });
+
+  // You can assign a different this object when calling an existing function. this refers to the current object, the calling object. With call, you can write a method once and then inherit it in another object, without having to rewrite the method for the new object.
 });
