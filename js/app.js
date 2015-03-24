9610a933280a77f62cb388f04d5333a7bf8e8834
t32k <ijok.ijok@gmail.com>
2015-03-23 23:22:54 -0700

    Prev

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(function() {
-  // $(parent).prepend(el);
-  // IE8+
+  // $(el).prev();
+  // IE9+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  var text = el.insertBefore(child, parent.firstChild);;
+  var text = el.previousElementSibling;
   console.log(text);
 });
