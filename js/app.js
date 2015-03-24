f35ca6c7eb48eb9f2a723d632f11d4217b269c57
t32k <ijok.ijok@gmail.com>
2015-03-23 22:14:25 -0700

    Get Style

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(function() {
-  // IE8+
-  // $('<div>').append($(el).clone()).html();
+  // IE9+
+  // $(el).css(ruleName);
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  var text = el.outerHTML;
+  var text = getComputedStyle(el)['color'];
   console.log(text);
 });
