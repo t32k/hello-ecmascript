56fcd6dc10a1f4fdf0d693e1938f29428d28f3fc
t32k <ijok.ijok@gmail.com>
2015-03-23 18:23:47 -0700

    POST

--- js/app.js
+++ js/app.js
@@ -1,20 +1,6 @@
 $(function() {
-  var request = new XMLHttpRequest();
-  request.open('GET', 'foo.json', true);
-
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      // Success!
-      var data = JSON.parse(request.responseText);
-    } else {
-      // We reached our target server, but it returned an error
-
-    }
-  };
-
-  request.onerror = function() {
-    // There was a connection error of some sort
-  };
-
-  request.send();
+  var request = nex XMLHttpRequest();
+  request.open('POST', '/my/url', true);
+  request.setRequestHeader('Content-Type',  'application/x-www-form-urlencoded; charset=UTF-8');
+  request.send(data);
 });
