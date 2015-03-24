746a54f78f02dd4945253c3c4913c1a16daf8de3
t32k <ijok.ijok@gmail.com>
2015-03-23 20:44:05 -0700

    After

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 $(function() {
   var el = document.getElementById('js-el');
 
-  if (el.classList) {
-    el.classList.add('foo');
-  } else {
-    el.className += ' ' + 'f00';
-  }
+  el.insertAdjacentHTML('afterend', 'Hi!')
 });
