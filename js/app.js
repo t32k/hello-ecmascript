f8b8b6c3a4c194ea42ae91aefd57eb60685fefdc
t32k <ijok.ijok@gmail.com>
2015-03-24 00:30:03 -0700

    Siblings

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 $(function() {
-  // $(el).text(string);
+  // $(el).siblings();
   // IE9+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  el.textContent = '!';
+  console.log(Array.prototype.filter.call(el.parentNode.children, function(child){
+    return child !== el;
+  }));
 });
