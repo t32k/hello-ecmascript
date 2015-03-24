d441ffc1039052d7f4f302cc58c915e1c7d5007f
t32k <ijok.ijok@gmail.com>
2015-03-23 22:30:52 -0700

    NExt

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 $(function() {
   // IE9+
-  // $(el).is('.my-class');;
+  // $(el).next();
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  var matches = function(el, selector) {
-    return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
-  };
-
-  matches(el, '.my-class');
+  var text = el.nextElementSibling;
+  console.log(text);
 });
