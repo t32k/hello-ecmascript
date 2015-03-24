73a7b42d6f4d8182dd02cf433fc4f675fe00e392
t32k <ijok.ijok@gmail.com>
2015-03-23 23:28:02 -0700

    Set HTML

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(function() {
-  // $(el).attr('tabindex', 3);
+  // $(el).html(string);
   // IE8+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  el.setAttribute('tabindex', 3);
+  el.innerHTML = '!';
 });
