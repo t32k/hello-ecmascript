fe9347b7485b30eafd0ce244f57d7291d090d21a
t32k <ijok.ijok@gmail.com>
2015-03-23 23:24:43 -0700

    Remove Class

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(function() {
-  // $(el).remove();
+  // $(el).removeClass(className);
   // IE8+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  el.parentNode.removeChild(el);
-
+  if (el.classList) {
+    el.classList.remove(className);
+  } else {
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
 });
