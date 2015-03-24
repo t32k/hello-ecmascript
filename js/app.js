118ce26dd17a8bcce91e14c7f8b20fbb49881306
t32k <ijok.ijok@gmail.com>
2015-03-23 21:41:08 -0700

    Find Elements

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(function() {
   // IE8+
-  // $(el).find(selector);
+  // $('.my #awesome selector');
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  el.querySelectorAll('#js-child');
+  document.querySelectorAll('.my #awesome selector');
 });
