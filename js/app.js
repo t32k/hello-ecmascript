f03dc4d545a5beb9739d4b375516ba14f6db81a3
t32k <ijok.ijok@gmail.com>
2015-03-24 00:40:05 -0700

    Off

--- js/app.js
+++ js/app.js
@@ -1,21 +1,12 @@
 $(function() {
-  // $(el).toggleClass(className);
+  // $(el).off(eventName, eventHandler);
   // IE9+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  if (el.classList) {
-    el.classList.toggle(className);
-  } else {
-    var classes = el.className.split(' ');
-    var existingIndex = classes.indexOf(className);
-
-    if (existingIndex >= 0)
-      classes.splice(existingIndex, 1);
-    else
-      classes.push(className);
-
-    el.className = classes.join(' ');
+  var eventHandler = function() {
+    console.log('Click!!!!!')
   }
+  el.removeEventListener('click', eventHandler);
 });
