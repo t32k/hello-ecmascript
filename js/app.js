a1a806b44dcb161b9551ac314c81979e2f7b5a30
t32k <ijok.ijok@gmail.com>
2015-03-23 23:28:58 -0700

    Set Text

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(function() {
-  // $(el).html(string);
-  // IE8+
+  // $(el).text(string);
+  // IE9+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  el.innerHTML = '!';
+  el.textContent = '!';
 });
