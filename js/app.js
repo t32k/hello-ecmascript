df889158732fbcc85491e4fb17c164368336d8cd
t32k <ijok.ijok@gmail.com>
2015-03-23 21:36:47 -0700

    Filter

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ $(function() {
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
+  // $(selector).filter(filterFn);
 
-  el.innerHTML = '';
+  Array.prototype.filter.call(items, function(e){
+    console.log(e);
+  });
+  // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
 });
