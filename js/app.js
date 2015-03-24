ac1e69e07bf42f9a3e718908c5d76a5f3df7b65b
t32k <ijok.ijok@gmail.com>
2015-03-23 18:16:54 -0700

    JSON

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 $(function() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'foo.json', true);
 
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      var data = JSON.parse(request.responseText);
+    } else {
+      // We reached our target server, but it returned an error
+
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+
+  request.send();
 });
