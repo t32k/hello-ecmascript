750123069c26a72eae9af04a07b98bf00632e85d
t32k <ijok.ijok@gmail.com>
2015-03-23 22:53:32 -0700

    Outer Height With Margin

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 $(function() {
-  // $(el).outerHeight();
-  // IE8+
+  // $(el).outerHeight(true);
+  // IE9+
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  var text = el.offsetHeight
+  function outerHeight(el) {
+    var height = el.offsetHeight;
+    var style = getComputedStyle(el);
+
+    height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+    return height;
+  }
+
+  var text = outerHeight(el);
   console.log(text);
 });
