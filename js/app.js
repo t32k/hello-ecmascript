9c4186f2d0f0e4d222185e927996bf041ac540c0
t32k <ijok.ijok@gmail.com>
2015-03-23 22:21:57 -0700

    Has Class

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 $(function() {
-  // IE9+
-  // $(el).text();
+  // IE8+
+  // $(el).hasClass(className);
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
 
-  var text = el.textContent;
+
+  if (el.classList) {
+    var text = el.classList.contains('foo');
+  } else {
+    var text = new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+  }
   console.log(text);
 });
