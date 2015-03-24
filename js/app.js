afb7774518a18db1764cee883cb8a5ef9da337fa
t32k <ijok.ijok@gmail.com>
2015-03-23 20:20:54 -0700

    Hide

--- js/app.js
+++ js/app.js
@@ -1,18 +1,4 @@
 $(function() {
-  function fadeIn(el) {
-    el.style.opacity = 0;
-
-    var last = +new Date();
-    var tick = function() {
-      el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
-      last = +new Date();
-
-      if (+el.style.opacity < 1) {
-        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
-      }
-    };
-    tick();
-  }
-
-  fadeIn(el);
+  var el = document.getElementById('js-el');
+  el.style.display = 'none';
 });
