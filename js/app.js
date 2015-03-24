8ee6625cc308fce3b2df651e8a40239bb21f4f68
t32k <ijok.ijok@gmail.com>
2015-03-23 21:38:28 -0700

    Find Children

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 $(function() {
-  // IE9+
+  // IE8+
+  // $(el).find(selector);
   var el = document.getElementById('js-el');
   var child = document.getElementById('js-child');
   var items = document.querySelectorAll('#js-list > li');
-  // $(selector).filter(filterFn);
 
-  Array.prototype.filter.call(items, function(e){
-    console.log(e);
-  });
-  // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+  el.querySelectorAll('#js-child');
 });
